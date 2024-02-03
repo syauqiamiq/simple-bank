243b38412187da4185efceb2e15c2e37763ae447
Syauqi Amiq <syauqiamiq12@gmail.com>
2024-02-03 23:33:13 +0700

    feat: tranfer tx and unit test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,13 +15,15 @@ const (
 )
 
 var testQueries *Queries
+var testDB *pgx.Conn
 
 func TestMain(m *testing.M) {
-	conn, err := pgx.Connect(context.Background(), dbSource)
+	var err error
+	testDB, err = pgx.Connect(context.Background(), dbSource)
 	if err != nil {
 		log.Fatal("Cannot connect to database: ", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 	os.Exit(m.Run())
 }
---
+++ db/sqlc/store.go
@@ -0,0 +1,95 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+// provide all function to execute db query and transaction
+type Store struct {
+	*Queries
+	db *pgx.Conn
+}
+
+func NewStore(db *pgx.Conn) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes a function within a database transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		rbErr := tx.Rollback(ctx)
+		if rbErr != nil {
+			return fmt.Errorf("tx err:%v, rb err:%v ", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit(ctx)
+}
+
+// this is input parameter of the transaction param
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// this is result of transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TransferTx performs a money transfer from one account to another account
+// it creates transfer record, add account entries, and update account balance with single database tx
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: Update Account Balance
+
+		return nil
+	})
+
+	return result, err
+}
